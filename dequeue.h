e25227bfc2dbf692287a3eaa9c2d52f1085e8242
kaveh Mehdipour <kavehmehdipour@yahoo.com>
2025-11-03 12:58:59 -0500

    Update dequeue.h for improved memory management
    Refactor dequeue.h to improve memory management and block size handling.

--- dequeue.h
+++ dequeue.h
@@ -1,8 +1,15 @@
 #include <utility>
-
+#include <vector>
 
 constexpr std::size_t   NUM_BLOCKS = 2;
-constexpr std::size_t   BLOCK_SIZE = 20;
+constexpr std::size_t   BLOCK_SIZE_NUM = 20;
+constexpr std::size_t   BLOCK_SIZE_BYTE = 120;
+
+//use reinterpret_cast and reserving raw bit to prevent default constructor destructor calling 
+//consider to change naming to map and buffer.
+//increasing the size when the controller and the buffer it is full
+//shrink to fit and decreasing the size
+//size indication 
 
 template <typename T>
 struct Controller{
@@ -13,17 +20,29 @@ struct Controller{
     Controller& operator=(const Controller& other) = delete;
     Controller& operator=(Controller&& other) noexcept;
     
-    std::size_t m_block_size;
-    std::size_t m_num_blocks;
+    //it is better to make this a function where there are variable size for class
+    std::size_t m_block_size = std::min(20 * sizeof(T), static_cast<std::size_t>(BLOCK_SIZE_BYTE / sizeof(T)));
+    std::size_t m_num_blocks = BLOCK_SIZE_BYTE;
+    std::vector<T*> m_block; //should be filled with nullptr? //it is a pointer to the map
+    T* m_mid_block;
+    T* m_tail_block;
+    T* m_head_block;
+    //index to memory formulas
     
 };
+
+
+
 
 
 template <typename T>
 class dequeue{
     public:
     dequeue();
-    ~dequeue();
+    explicit dequeue(std::size_t size);
+    explicit dequeue(std::size_t size, const T& value);
+    dequeue(T* start_ptr, T* end_ptr);
+    ~dequeue() noexcept;
     dequeue(const dequeue& other) = delete;
     dequeue(dequeue&& other) noexcept;
     dequeue& operator=(const dequeue& other) = delete;
